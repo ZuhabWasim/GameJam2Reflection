ad53a0e5eeaff4274646f52a8c0298e6851a18e8
flryang <florayangmeilin@gmail.com>
2022-01-29 12:11:03 -0500

    player

--- Assets/scripts/CameraScript.cs
+++ Assets/scripts/CameraScript.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-
+    public float mouseSensitivity = 200f;
+    public Transform playerBody;
+    float xRotation = 0.0f;
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch += speedV * Input.GetAxis("Mouse Y");
-
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
